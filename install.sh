2ee1633e79b5a8738f03c73a7bd9b16e7f5881f3
mkantonio <39425186+mkantonio@users.noreply.github.com>
2022-06-25 11:36:55 -0400

    install-script
    bash post install

---
+++ install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+sudo apt-get update -y
+sudo apt install -y curl wget
+sudo apt-get install zsh -y
+zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+sudo apt-get install rar -y
+sudo apt-get install unrar -y
+sudo apt-get install curl -y
+sudo apt install nload -y
+sudo apt install snapd -y
+sudo snap install jdownloader2 -y
+sudo snap install telegram-desktop -y
+sudo apt install -y python3-pip
+sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
+sudo apt install -y python3-venv
+python3 -m pip install pipenv
+sudo apt-get install ffmpeg -y
