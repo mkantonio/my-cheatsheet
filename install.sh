a4df7f6b5a77163881854752116dd86c811ddce0
mkantonio <39425186+mkantonio@users.noreply.github.com>
2022-06-26 12:20:28 -0400

    Update install.sh
    update commands

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
 sudo apt-get update -y
-sudo apt install -y curl wget
-sudo apt-get install zsh -y
-zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
-sudo apt-get install rar -y
-sudo apt-get install unrar -y
-sudo apt-get install curl -y
-sudo apt install nload -y
-sudo apt install snapd -y
-sudo snap install jdownloader2 -y
-sudo snap install telegram-desktop -y
+sudo apt install -y curl wget rar unrar nload snapd
 sudo apt install -y python3-pip
 sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
 sudo apt install -y python3-venv
 python3 -m pip install pipenv
 sudo apt-get install ffmpeg -y
+
+sudo apt install tasksel -y
+sudo apt-get install kubuntu-desktop^ -y
+
+#sudo apt-get install zsh -y
+#zsh
+#sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+#
+#sudo snap install jdownloader2 -y
+#sudo snap install telegram-desktop -y
+
